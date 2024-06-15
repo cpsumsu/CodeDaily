0cbd21a92f5d155e40369f668b6f33b1d619e8b9
tom-choi <cvb233299@gmail.com>
2024-06-15 23:11:18 +0800

    fix some of the re

--- ActionPy/SetUpForTest/setupfortest.py
+++ ActionPy/SetUpForTest/setupfortest.py
@@ -1,6 +1,9 @@
 import os
-markdown_files = [os.path.join('leetcode', f) for f in os.listdir('leetcode') if f.endswith('.md')]
+
+markdown_files = [os.path.join('leetcode/', f) for f in os.listdir('leetcode') if f.endswith('.md')]
 print(markdown_files)
+
+# https://regex101.com/r/1lTwsY/1
 
 with open('ActionPy/test_metadata.py', 'w+', encoding='utf-8') as file:
     file.write('import pytest\n')
@@ -8,33 +11,34 @@ with open('ActionPy/test_metadata.py', 'w+', encoding='utf-8') as file:
     file.write(
 """
 import re
-import pytest
 class TestBase:
     def metadata_format(file_path):
         \"\"\"
         Check if the given Markdown file has the expected metadata format.
         \"\"\"
-        with open(file_path, 'r') as f:
+        with open(file_path, 'r', encoding='utf-8') as f:
             content = f.read()
             
             # Check if the file starts with the expected metadata format
-            metadata = re.match(r'^---\\nDifficulty: "(\w+)"\\ntags: (\[.*?\])\\n(.*?)\\n---\\n', content)
+            metadata = re.match(r'---\\nDifficulty: "(?P<difficulty>\w+)"\\ntags: \[(?P<tags>[\w, "]+)\](\\ntitle: "(?P<title>\w+)")?\\n---', content)
             assert metadata is not None, f'Metadata format is missing or incorrect in {file_path}'
             
             difficulty = metadata.group(1)
             tags = metadata.group(2)
-            extra_properties = metadata.group(3).strip().split('\\n')
+            extra_properties = metadata.group(3)
             
             # Check if Difficulty and tags are present
             assert difficulty is not None, f'Difficulty is missing in {file_path}'
             assert tags is not None, f'Tags is missing in {file_path}'
             
             # Check for extra properties
-            for prop in extra_properties:
-                if prop.startswith('name:'):
-                    pass  # Ignore the "name" property
-                else:
-                    assert False, f'Unexpected property "{prop.split(":")[0]}" in {file_path}'
+            if extra_properties is not None:
+                extra_properties = extra_properties.strip().split('\\n')
+                for prop in extra_properties:
+                    if prop.startswith('title:'):
+                        pass  # Ignore the "title" property
+                    else:
+                        assert False, f'Unexpected property "{prop.split(":")[0]}" in {file_path}'
 """)
 
     for i, file_path in enumerate(markdown_files):
@@ -45,3 +49,18 @@ class TestBase:
         file.write('    TestBase.metadata_format(file_path)\n\n')
 
 print('test_metadata.py file created successfully!')
+
+# const text = `
+# ---
+# Difficulty: "Easy"
+# tags: ["枚舉"]
+# ---
+
+# > Problem: [1696. 跳跃游戏 VI](https://leetcode.cn/problems/jump-game-vi/description/)
+# `;
+
+# const regex = /---\nDifficulty: "(?<difficulty>\w+)"\ntags: \[(?<tags>[\w, "]+)\]\n---/;
+# const match = text.match(regex);
+
+# console.log(match.groups.difficulty); // "Easy"
+# console.log(match.groups.tags); // '"枚舉"'

e6111c8989ec40fb21fef388cadc10ee51f7eeb6
tom-choi <cvb233299@gmail.com>
2024-06-15 22:19:06 +0800

    Delete test_metadata.py

--- ActionPy/test_metadata.py
+++
@@ -1,6 +0,0 @@
-from .SetUpForTest.TestBase import TestBase
-import pytest
-
-@pytest.mark.parametrize("file_path", "leetcode\\3. 无重复字符的最长子串.md")
-def test_1(file_path):
-    TestBase.metadata_format(file_path)
